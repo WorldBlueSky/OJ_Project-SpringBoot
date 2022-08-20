f2c3e88e89930ee09d245e1328258d376141b702
rain7-7 <2745131427@qq.com>
2022-08-20 14:12:00 +0800

    解决了跨域问题

---
+++ src/main/java/com/example/demo/config/CrossConfig.java
@@ -0,0 +1,57 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * 用来解决发生的跨域问题
+ */
+
+@Configuration
+public class CrossConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        // 允许cookies跨域
+        config.setAllowCredentials(true);
+
+        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.setMaxAge(18000L);
+
+        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedOrigin("*");
+
+        // #允许访问的头信息,*表示全部
+        config.addAllowedHeader("*");
+
+        // 允许提交请求的方法类型，*表示全部允许
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+
+        config.addExposedHeader("set-cookie");
+        config.addExposedHeader("access-control-allow-headers");
+        config.addExposedHeader("access-control-allow-methods");
+        config.addExposedHeader("access-control-allow-origin");
+        config.addExposedHeader("access-control-max-age");
+        config.addExposedHeader("X-Frame-Options");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
